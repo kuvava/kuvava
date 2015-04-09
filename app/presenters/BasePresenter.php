668c22355a1b97eb2b070e94527c524be1d222a2
Tomáš Urban <urbanovi@kuvava.cz>
2015-04-09 06:38:27 +0200

    add basic connection with database and basic layout for content

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -11,5 +11,38 @@ use Nette,
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
-
+	/** @var Nette\Database\Context */
+	protected $database;
+	public function injectDatabase(Nette\Database\Context $database)
+	{
+		$this->database = $database;
+	}
+	protected function getStranka(){
+		$idStranek = array(
+			'Homepage' => 1,
+			'OsobniPromenyKontakt' => 2,
+			'ModerniPromenaVizaze' => 3,
+			'TeamVizazFoceni' => 4,
+			'PromenaVzhledu' => 5,
+			'ZmenaImageZeny' => 6,
+			'ZmenaImageMuze' => 7
+			);
+		$stranka = $this->database->table('stranka')->get($idStranek[$this->name]);
+		if ($stranka) {
+			return $stranka;
+		} else {
+			$this->shootError();
+		}
+	}
+	protected function shootError($message = 'Omlouváme se, ale stránku nelze nalézt.<br>Kontaktujte prosím správce webu: urbanovi&#64;<!-- -->kuvava.cz<br>nebo si vyberte jiný obsah v menu.', $class = 'fr', $errorText = 'Odkazovaný obsah nelze nalézt.')
+	{
+		$this->flashMessage($message,$class);
+		$this->error($errorText);
+	}
+	public function beforeRender()
+	{
+		if (!in_array($this->name, array('Error','Nastroje'), TRUE)){
+			$this->template->stranka = $this->getStranka();
+		}
+	}
 }
