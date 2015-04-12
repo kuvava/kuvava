4ac495651aa76be13793c7deba94a7fab1de0958
Tomáš Urban <urbanovi@kuvava.cz>
2015-04-12 17:30:46 +0200

    Create action Nastroje:zpracujMiniatury

--- app/presenters/NastrojePresenter.php
+++ app/presenters/NastrojePresenter.php
@@ -4,7 +4,8 @@ namespace App\Presenters;
 
 use Nette,
 	App\Model,
-	Nette\Utils\Finder;
+	Nette\Utils\Finder,
+	Nette\Utils\Image;
 	
 
 
@@ -37,9 +38,11 @@ class NastrojePresenter extends BasePresenter
 			$size = $value->getSize();
 			$fileextension = Nette\Utils\Strings::lower($value->getExtension());
 			$tvar = 'neznamy';
-			if ($width > $height) {$tvar = 'nasirku';}
-			elseif ($width < $height) {$tvar = 'navysku';}
-			elseif ($width = $height) {$tvar = 'ctverec';}
+			if ($width > $height) {$tvar = 'nasirku';
+			} elseif ($width < $height) {$tvar = 'navysku';
+			} elseif ($width == $height) {$tvar = 'ctverec';
+			}
+			
 			$insertion = array(
 				'size' => $size,
 				'width' => $width,
@@ -56,7 +59,7 @@ class NastrojePresenter extends BasePresenter
 			$success = rename($key, $parentDir . '/../all/' . $filename);
 			if ($success) {
 				$this->flashMessage('Přenos souboru <strong><a href="/images/gallery/all/' . $filename .'" target="_blank">' . $filename . '</a></strong> se zdařil.');
-				$this->redirect('Nastroje:zpracujMiniatury', $photoId, 'l',$filename,$tvar);
+				$this->redirect('Nastroje:zpracujMiniatury', $photoId, 'l',$filename);
 			} else {
 				$this->flashMessage('Přenos souboru <strong>' . $filename . '</strong> se nezdařil.', 'fr');
 				$this->redirect('Nastroje:');
@@ -66,16 +69,59 @@ class NastrojePresenter extends BasePresenter
 		$this->flashMessage('Ve složce <strong>to-do/</strong> již nejsou žádné další <strong>.jpg</strong> soubory ke zpracování.');
 		$this->redirect('Nastroje:');
 	}
-	public function actionZpracujMiniatury($photoId,$sizeCat,$filename,$tvar)
+	public function actionZpracujMiniatury($photoId,$sizeCat,$filename)
 	{
 		if (!$this->user->isInRole('admin')){
 			$this->flashMessage('Pro přístup do sekce "nástroje" musíte být přihlášen jako uživatel s administrátorskými právy.','fr');
 			$this->redirect('Sign:in');
 		}
+		$resizeDimension = 1200;
+		if ($sizeCat == 'l') {$resizeDimension = 1200;
+		} elseif ($sizeCat == 'm') {$resizeDimension = 800;
+		} elseif ($sizeCat == 's') {$resizeDimension = 200;
+		}
 		
+		$imagesDir =  __DIR__ . '/../../www/images/gallery/all/';
+		$currentSourceFile = $imagesDir . $filename;
+		if ($sizeCat == 'l') {$currentSourceFile = $imagesDir . $filename;
+		} elseif ($sizeCat == 'm') {$currentSourceFile = $imagesDir . 'thumbs/l/' . $filename;
+		} elseif ($sizeCat == 's') {$currentSourceFile = $imagesDir . 'thumbs/m/' . $filename;
+		}
 		
+		\Tracy\Debugger::barDump('for size:' . $sizeCat . ', resize source:' . $currentSourceFile);
+		$image = Image::fromFile($currentSourceFile);
+		if ($sizeCat == 's'){
+			$image->resize($resizeDimension, $resizeDimension, Image::SHRINK_ONLY | Image::FILL);
+		} else {
+			$image->resize($resizeDimension, $resizeDimension, Image::SHRINK_ONLY);
+		}
+		$image->sharpen();
+		$partUrlResizedFile = 'thumbs/' . $sizeCat . '/' . $filename;
+		$resizedFile = $imagesDir . $partUrlResizedFile;
+		$image->save($resizedFile, 70, Image::JPEG);
+		list($width, $height, $type, $html_attr) = getimagesize($resizedFile);
+		$size = filesize($resizedFile);
 		
-		$this->redirect('Nastroje:zpracujFotky');
+		if ($size > 0 && $width > 0 && $height > 0) {
+			$updating = array(
+				$sizeCat . '_size' => $size,
+				$sizeCat . '_width' => $width,
+				$sizeCat . '_height' => $height,
+				$sizeCat . '_html_attr' => $html_attr,
+			);
+			$this->database->table('vsechny_fotky')->get($photoId)->update($updating);
+			$this->flashMessage('Výroba miniatury <strong><a href="/images/gallery/all/' . $partUrlResizedFile .'" target="_blank">' . $partUrlResizedFile . '</a></strong> se zdařila.');
+			
+			if ($sizeCat == 's') {
+				$this->redirect('Nastroje:zpracujFotky');
+			} else {
+				$targetSizeCat = ($sizeCat == 'l' ? 'm' : 's');
+				$this->redirect('Nastroje:zpracujMiniatury', $photoId, $targetSizeCat, $filename);
+			}	
+		} else {
+			$this->flashMessage('Při výrobě miniatury<strong>' . $partUrlResizedFile . '</strong>došlo k chybě.', 'fr');
+			$this->redirect('Nastroje:');
+		}
 	}
 
 }
