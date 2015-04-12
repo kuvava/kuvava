e2c1cabbfb39c059639b45ef9344222fcaf5ef41
Tomáš Urban <urbanovi@kuvava.cz>
2015-04-12 12:18:23 +0200

    Final first version of stahuj.fotourban.cz

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -3,7 +3,8 @@
 namespace App\Presenters;
 
 use Nette,
-	App\Model;
+	App\Model,
+	Nette\Utils\Finder;
 
 
 /**
@@ -12,5 +13,21 @@ use Nette,
 class HomepagePresenter extends BasePresenter
 {
 
+	public function beforeRender()
+	{
+		$parentDir =  __DIR__ . '/../../www/download';
+		$rootZip = Finder::findFiles('*.zip')->in($parentDir);
+		$subdirectories = Finder::findDirectories('*')->in($parentDir);
+		$subdirZip = array();
+		foreach ($subdirectories as $key => $value) {
+			$zipFiles = array();
+			$zipFiles = Finder::findFiles('*.zip')->in($key);
+			if (count($zipFiles) > 0) {
+				$subdirZip[$value->getFilename()] = $zipFiles;
+			}
+		}
+		$this->template->rootZip = $rootZip;
+		$this->template->subdirZip = $subdirZip;
+	}
 
 }
